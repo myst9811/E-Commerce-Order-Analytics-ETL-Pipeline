49253faea08f901a4ffa94cdece122bc4be1ef33
Shannen Saikia <shannen.saikia@gmail.com>
2026-02-11 00:32:32 +0530

    feat: creating Silver Customers (SCD Type 2)

---
+++ 2_SILVER/2_SILVER_CREATE.sql
@@ -0,0 +1,32 @@
+-- Silver layer table creation
+-- File: 2_SILVER_CREATE.sql
+-- Purpose: Cleansed & conformed tables (SCD Type 2 – Customers)
+-- Idempotent: yes
+
+USE ROLE INTERN_ROLE;
+USE DATABASE INTERN_DB;
+USE SCHEMA INTERNS_SANDBOX;
+USE WAREHOUSE INTERN_WH;
+
+
+-- Silver customers (SCD Type 2)
+CREATE TABLE IF NOT EXISTS SS_SILVER_CUSTOMERS (
+    customer_sk NUMBER AUTOINCREMENT,
+    customer_id NUMBER,
+
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(255),
+    city VARCHAR(100),
+    country VARCHAR(100),
+    customer_segment VARCHAR(50),
+
+    effective_from DATE,
+    effective_to DATE,
+    is_current BOOLEAN,
+
+    row_hash VARCHAR(64),
+
+    created_ts TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_ts TIMESTAMP_NTZ
+);
